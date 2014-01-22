0d0efd5fb9df49b822daf96a1a8999b830ebe4d7
jringenberg <joe@wistia.com>
2014-01-22 17:04:54 -0500

    first d3 experiment

---
+++ assets/js/experiments.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+  var data = [4, 2, 18, 16, 23, 42];
+
+  var x = d3.scale.linear()
+      .domain([0, d3.max(data)])
+      .range([0, 420]);
+
+  d3.select(".first-chart")
+    .selectAll("div")
+      .data(data)
+    .enter().append("div")
+      .style("width", function(d) { return x(d) + "px"; })
+      .text(function(d) { return d; });
+
+});
