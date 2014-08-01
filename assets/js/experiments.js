ffbc53d223385edff9b06cf70a3ecb75e4fc4fd6
jringenberg <joe@wistia.com>
2014-08-01 13:11:40 -0400

    update public posts

--- assets/js/experiments.js
+++ assets/js/experiments.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 
-  var data = [4, 2, 18, 16, 23, 42];
+  var data = [4, 2, 18, 16, 23, 48];
 
   var x = d3.scale.linear()
       .domain([0, d3.max(data)])
-      .range([0, 420]);
+      .range([0, 600]);
 
   d3.select(".first-chart")
     .selectAll("div")
@@ -12,5 +12,7 @@ $(document).ready(function(){
     .enter().append("div")
       .style("width", function(d) { return x(d) + "px"; })
       .text(function(d) { return d; });
+
+
 
 });
